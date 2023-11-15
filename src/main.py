b628d445bf872db66e5d37f1dd5544082db5492b
Michal Placzek <placzu.com@gmail.com>
2023-11-15 18:57:29 +0100

    add Test to class ReturnClass

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-
+import unittest
 
 
 class ReturnClass:
@@ -8,13 +8,14 @@ back = ReturnClass()
 resolution = back.return_number()
 print(f"number = {resolution}")
 
+class Test_ReturnClass(unittest.TestCase):
+    def test_return_number(self):
+        back = ReturnClass()
+        test_resolution = back.return_number()
+        self.assertEqual(test_resolution, 897)
 
-
-
-
-
-
-
+if __name__ == "__main__":
+    unittest.main()
 
 
 
