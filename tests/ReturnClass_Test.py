2a0c315cec9591e523db99481777dc3796fab882
Michal Placzek <placzu.com@gmail.com>
2023-11-17 09:28:15 +0100

    separated into Test and Main File

--- tests/ReturnClass_Test.py
+++ tests/ReturnClass_Test.py
@@ -2,6 +2,6 @@ import unittest
 
 class Test_ReturnClass(unittest.TestCase):
     def test_return_number(self):
-        back = ReturnClass()
-        test_resolution = back.return_number()
-        self.assertEqual(test_resolution, 897)
+        example = ReturnClass()
+        actual_value = example.return_number()
+        self.assertEqual(actual_value, 897)
