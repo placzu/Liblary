9347ab5a7861d6f58cfd4a72b343fb8f5e7886d9
Michal Placzek <placzu.com@gmail.com>
2023-11-24 07:57:32 +0100

    add default values for book, test with missing data fixed

--- src/book.py
+++ src/book.py
@@ -1,11 +1,13 @@
 class Book:
 
-    def __init__(self, name: str, author: str, short_description: str, pages: int):
+    def __init__(self, name: str = "Unknow", author: str = "Unknown", short_description: str = "No description", pages: int = 0):
         self.name = name
         self.author = author
         self.short_description = short_description
         self.pages = pages
 
     def present(self):
+
+
         return (
             f"Book entitled {self.name} written by: {self.author}, with a short review {self.short_description} and {self.pages} pages.")
--- tests/unit_test/test_book.py
+++ tests/unit_test/test_book.py
@@ -2,15 +2,18 @@ import unittest
 from src.book import Book
 
 
+def setUp():
+    testemptybook = Book()
+
 class TestBook(unittest.TestCase):
 
-    def test_book_present(self):
-        self.testbook = Book(name="Biography of John", author="John Grimmes", short_description="John's life story",
-                             pages=264)
+    def testbook_present(self):
+        testbook = Book(name="Biography of John", author="John Grimmes", short_description="John's life story",
+                        pages=264)
         expected_output = "Book entitled Biography of John written by: John Grimmes, with a short review John's life story and 264 pages."
-        self.assertEqual(self.testbook.present(), expected_output)
+        self.assertEqual(testbook.present(), expected_output)
 
-    def test_empty_book_present(self):
-        self.testemptybook = Book(name="", author="", short_description="", pages=0)
-        expected_output = "Book entitled  written by: , with a short review  and 0 pages."
-        self.assertEqual(self.testemptybook.present(), expected_output)
+    def testemptybook_present(self):
+        testemptybook = Book(name="Best composer", author="Rok Nardin", pages=57)
+        expected_solution = "Book entitled Best composer written by: Rok Nardin, with a short review No description and 57 pages."
+        self.assertEqual(testemptybook.present(), expected_solution)
